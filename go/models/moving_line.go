c0ceef1b3bbcddaab19188b8c2c1bcab0ba9a8cf
peugeot <thomaspeugeot@yahoo.fr>
2024-12-25 10:06:28 +0100

    Add a cursor #7 - remove the animation bar from the gongsvg stack

--- go/models/moving_line.go
+++ go/models/moving_line.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"fmt"
 	"math"
 
 	gongsvg_models "github.com/fullstack-lang/gongsvg/go/models"
@@ -29,8 +28,9 @@ func (movingline *MovingLine) Draw(gongsvgStage *gongsvg_models.StageStruct,
 	parameter *Parameter,
 ) {
 	themeWidth := parameter.RotatedAxis.Length
+	_ = themeWidth
 
-	line := new(gongsvg_models.Line).Stage(gongsvgStage)
+	line := new(gongsvg_models.Line)
 	line.Name = movingline.Name
 	layer.Lines = append(layer.Lines, line)
 
@@ -42,27 +42,4 @@ func (movingline *MovingLine) Draw(gongsvgStage *gongsvg_models.StageStruct,
 	line.X2 = line.X1 + movingline.Length*math.Cos(angleRad)
 	line.Y2 = line.Y1 - movingline.Length*math.Sin(angleRad)
 
-	if movingline.IsMoving {
-		{
-			animateX1 := new(gongsvg_models.Animate).Stage(gongsvgStage)
-			animateX1.AttributeName = "x1"
-			animateX1.Dur = fmt.Sprintf("%0.3fs", movingline.DurationSeconds)
-			animateX1.Values = fmt.Sprintf("%f;%f", line.X1, line.X1+themeWidth)
-			animateX1.RepeatCount = `indefinite`
-
-			line.Animates = append(line.Animates, animateX1)
-		}
-		{
-			animateX2 := new(gongsvg_models.Animate).Stage(gongsvgStage)
-			animateX2.AttributeName = "x2"
-			animateX2.Dur = fmt.Sprintf("%0.3fs", movingline.DurationSeconds)
-			animateX2.Values = fmt.Sprintf("%f;%f", line.X1, line.X1+themeWidth)
-			animateX2.RepeatCount = `indefinite`
-
-			line.Animates = append(line.Animates, animateX2)
-		}
-
-	}
-
-	movingline.Presentation.CopyTo(&line.Presentation)
 }
