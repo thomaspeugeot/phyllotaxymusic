e09cdf04154d3a05615070f57bc6a84f6df05fd7
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-18 18:31:36 +0100

    new gongc

--- sliders/go/models/models.go
+++ sliders/go/models/models.go
@@ -63,3 +63,14 @@ type Checkbox struct {
 type CheckboxProxyInterface interface {
 	Updated()
 }
+
+func (checkbox *Checkbox) OnAfterUpdate(
+	stage *StageStruct,
+	stageCheckbox, frontCheckbox *Checkbox) {
+
+	checkbox.ValueBool = frontCheckbox.ValueBool
+
+	if checkbox.Proxy != nil {
+		checkbox.Proxy.Updated()
+	}
+}
