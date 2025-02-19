a697b557b9f8e4185fc91cd628d7f1e7db7a4984
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-02-19 08:46:04 +0100

    checkbox on proxy

--- go/models/checkbox_proxy.go
+++ go/models/checkbox_proxy.go
@@ -1,20 +1,18 @@
 package models
 
 import (
-	"log"
-
 	m "github.com/thomaspeugeot/phyllotaxymusic/sliders/go/models"
 )
 
 // Generic checkbox creation function
 func NewCheckbox(
-	parameter *Parameter,
+	target Target,
 	name string,
 	labelForTrue string,
 	labelForFalse string,
 	valueRef *bool,
 ) *m.Checkbox {
-	checkbox := new(m.Checkbox).Stage(parameter.slidersStage)
+	checkbox := new(m.Checkbox).Stage(target.GetSliderStage())
 	checkbox.Name = name
 	checkbox.LabelForTrue = labelForTrue
 	checkbox.LabelForFalse = labelForFalse
@@ -23,7 +21,7 @@ func NewCheckbox(
 	proxy := NewCheckboxProxy(
 		checkbox,
 		valueRef,
-		parameter,
+		target,
 	)
 
 	checkbox.Proxy = proxy
@@ -31,36 +29,29 @@ func NewCheckbox(
 	return checkbox
 }
 
+// NewCheckboxProxy creates a new proxy for a checkbox
+func NewCheckboxProxy(
+	checkbox *m.Checkbox,
+	value *bool,
+	target Target,
+) *CheckboxProxy {
+	proxy := new(CheckboxProxy)
+	proxy.checkbox = checkbox
+	proxy.Value = value
+	proxy.target = target
+	return proxy
+}
+
 // CheckboxProxy is a generic proxy for both int and float64
 type CheckboxProxy struct {
-	checkbox  *m.Checkbox
-	Value     *bool
-	parameter *Parameter
+	checkbox *m.Checkbox
+	Value    *bool
+	target   Target
 }
 
 // Updated handles updating values when the checkbox changes
 func (proxy *CheckboxProxy) Updated() {
 	*proxy.Value = proxy.checkbox.ValueBool
 
-	log.Println(proxy.parameter.SideLength)
-
-	proxy.parameter.UpdatePhyllotaxyStage()
-	proxy.parameter.UpdateAndCommitCursorStage()
-	proxy.parameter.UpdateAndCommitSVGStage()
-	proxy.parameter.UpdateAndCommitToneStage()
-	proxy.parameter.treeProxy.UpdateAndCommitTreeStage()
-	proxy.parameter.CommitPhyllotaxymusicStage()
-}
-
-// NewCheckboxProxy creates a new proxy for a checkbox
-func NewCheckboxProxy(
-	checkbox *m.Checkbox,
-	value *bool,
-	parameter *Parameter,
-) *CheckboxProxy {
-	proxy := new(CheckboxProxy)
-	proxy.checkbox = checkbox
-	proxy.Value = value
-	proxy.parameter = parameter
-	return proxy
+	proxy.target.UpdateAllStages()
 }
