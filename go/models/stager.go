30f46a7e63974a0c5b91d8cdf135910388547e63
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-03-26 05:55:07 +0100

    new gongc

--- go/models/stager.go
+++ go/models/stager.go
@@ -84,89 +84,85 @@ func NewStager(r *gin.Engine, stage *StageStruct) (stager *Stager) {
 			(&split.AsSplitArea{
 				Size:             50,
 				ShowNameInHeader: false,
-				AsSplits: []*split.AsSplit{
-					(&split.AsSplit{
-						Direction: split.Horizontal,
-						AsSplitAreas: []*split.AsSplitArea{
+				AsSplit: (&split.AsSplit{
+					Direction: split.Horizontal,
+					AsSplitAreas: []*split.AsSplitArea{
 
-							// Sidebar
-							(&split.AsSplitArea{
-								ShowNameInHeader: false,
-								Size:             15,
-								Tree: (&split.Tree{
-									StackName: stager.treeStage.GetName(),
-									TreeName:  Sidebar.ToString(),
-								}).Stage(stager.splitStage),
+						// Sidebar
+						(&split.AsSplitArea{
+							ShowNameInHeader: false,
+							Size:             15,
+							Tree: (&split.Tree{
+								StackName: stager.treeStage.GetName(),
+								TreeName:  Sidebar.ToString(),
 							}).Stage(stager.splitStage),
+						}).Stage(stager.splitStage),
 
-							// SVG area
-							(&split.AsSplitArea{
-								ShowNameInHeader: false,
-								Size:             65,
-								HasDiv:           true,
-								DivStyle:         "position: relative; width: 100%; height: 100%;",
-								Svg: (&split.Svg{
-									StackName: stager.svgStage.GetName(),
-									Style:     "position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: 1;",
-								}).Stage(stager.splitStage),
-								Cursor: (&split.Cursor{
-									StackName: stager.cursorStage.GetName(),
-									Style:     "position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: 2;pointer-events: none;",
-								}).Stage(stager.splitStage),
+						// SVG area
+						(&split.AsSplitArea{
+							ShowNameInHeader: false,
+							Size:             65,
+							HasDiv:           true,
+							DivStyle:         "position: relative; width: 100%; height: 100%;",
+							Svg: (&split.Svg{
+								StackName: stager.svgStage.GetName(),
+								Style:     "position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: 1;",
 							}).Stage(stager.splitStage),
+							Cursor: (&split.Cursor{
+								StackName: stager.cursorStage.GetName(),
+								Style:     "position: absolute;top: 0;left: 0;width: 100%;height: 100%;z-index: 2;pointer-events: none;",
+							}).Stage(stager.splitStage),
+						}).Stage(stager.splitStage),
 
-							// Right area
-							(&split.AsSplitArea{
-								ShowNameInHeader: false,
-								Size:             20,
-								AsSplits: []*split.AsSplit{
-									(&split.AsSplit{
-										Direction: split.Vertical,
-										AsSplitAreas: []*split.AsSplitArea{
+						// Right area
+						(&split.AsSplitArea{
+							ShowNameInHeader: false,
+							Size:             20,
+							AsSplit: (&split.AsSplit{
+								Direction: split.Vertical,
+								AsSplitAreas: []*split.AsSplitArea{
 
-											// Slider area
-											(&split.AsSplitArea{
-												ShowNameInHeader: false,
-												Size:             60,
-												Slider: (&split.Slider{
-													StackName: stager.sliderStage.GetName(),
-												}).Stage(stager.splitStage),
-											}).Stage(stager.splitStage),
+									// Slider area
+									(&split.AsSplitArea{
+										ShowNameInHeader: false,
+										Size:             60,
+										Slider: (&split.Slider{
+											StackName: stager.sliderStage.GetName(),
+										}).Stage(stager.splitStage),
+									}).Stage(stager.splitStage),
 
-											// Button area
-											(&split.AsSplitArea{
-												ShowNameInHeader: false,
-												Size:             20,
-												Button: (&split.Button{
-													StackName: stager.buttonStage.GetName(),
-												}).Stage(stager.splitStage),
-											}).Stage(stager.splitStage),
+									// Button area
+									(&split.AsSplitArea{
+										ShowNameInHeader: false,
+										Size:             20,
+										Button: (&split.Button{
+											StackName: stager.buttonStage.GetName(),
+										}).Stage(stager.splitStage),
+									}).Stage(stager.splitStage),
 
-											// Tone area
-											(&split.AsSplitArea{
-												ShowNameInHeader: false,
-												Size:             20,
-												Tone: (&split.Tone{
-													StackName: stager.toneStage.GetName(),
-												}).Stage(stager.splitStage),
-											}).Stage(stager.splitStage),
+									// Tone area
+									(&split.AsSplitArea{
+										ShowNameInHeader: false,
+										Size:             20,
+										Tone: (&split.Tone{
+											StackName: stager.toneStage.GetName(),
+										}).Stage(stager.splitStage),
+									}).Stage(stager.splitStage),
 
-											// load stage is load but invisible (Size =0). It
-											// is used to download generated Musescore files
-											(&split.AsSplitArea{
-												ShowNameInHeader: false,
-												Size:             0,
-												Load: (&split.Load{
-													StackName: stager.loadStage.GetName(),
-												}).Stage(stager.splitStage),
-											}).Stage(stager.splitStage),
-										},
+									// load stage is load but invisible (Size =0). It
+									// is used to download generated Musescore files
+									(&split.AsSplitArea{
+										ShowNameInHeader: false,
+										Size:             0,
+										Load: (&split.Load{
+											StackName: stager.loadStage.GetName(),
+										}).Stage(stager.splitStage),
 									}).Stage(stager.splitStage),
 								},
 							}).Stage(stager.splitStage),
-						},
-					}).Stage(stager.splitStage),
-				},
+						}).Stage(stager.splitStage),
+					},
+				}).Stage(stager.splitStage),
 			}).Stage(stager.splitStage),
 		},
 	}).Stage(stager.splitStage)
