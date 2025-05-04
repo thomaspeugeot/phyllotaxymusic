ab98a21f2cbff7e20c1aa7c290507663d1bdacd6
Thomas PEUGEOT <thomaspeugeot@yahoo.fr>
2025-05-05 01:48:01 +0200

    Before solving collisions between stacks

--- go/diagrams/diagrams.go
+++ go/diagrams/diagrams.go
@@ -23,15 +23,128 @@ func _(stage *models.Stage) {
 
 	// Declaration of instances to stage
 
+	__AttributeShape__000000_N := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000001_M := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000002_Z := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000003_InsideAngle := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000004_PitchDifference := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000005_BeatsPerSecond := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000006_IsMinor := (&models.AttributeShape{}).Stage(stage)
+	__AttributeShape__000007_ThemeBinaryEncoding := (&models.AttributeShape{}).Stage(stage)
+
+	__Classdiagram__000000_Default := (&models.Classdiagram{}).Stage(stage)
+
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z := (&models.DiagramPackage{}).Stage(stage)
 
+	__GongStructShape__000000_Default_Parameter := (&models.GongStructShape{}).Stage(stage)
+
 	// Setup of values
+
+	__AttributeShape__000000_N.Name = `N`
+
+	//gong:ident [ref_models.Parameter.N] comment added to overcome the problem with the comment map association
+	__AttributeShape__000000_N.Identifier = `ref_models.Parameter.N`
+	__AttributeShape__000000_N.FieldTypeAsString = ``
+	__AttributeShape__000000_N.Structname = `Parameter`
+	__AttributeShape__000000_N.Fieldtypename = `int`
+
+	__AttributeShape__000001_M.Name = `M`
+
+	//gong:ident [ref_models.Parameter.M] comment added to overcome the problem with the comment map association
+	__AttributeShape__000001_M.Identifier = `ref_models.Parameter.M`
+	__AttributeShape__000001_M.FieldTypeAsString = ``
+	__AttributeShape__000001_M.Structname = `Parameter`
+	__AttributeShape__000001_M.Fieldtypename = `int`
+
+	__AttributeShape__000002_Z.Name = `Z`
+
+	//gong:ident [ref_models.Parameter.Z] comment added to overcome the problem with the comment map association
+	__AttributeShape__000002_Z.Identifier = `ref_models.Parameter.Z`
+	__AttributeShape__000002_Z.FieldTypeAsString = ``
+	__AttributeShape__000002_Z.Structname = `Parameter`
+	__AttributeShape__000002_Z.Fieldtypename = `int`
+
+	__AttributeShape__000003_InsideAngle.Name = `InsideAngle`
+
+	//gong:ident [ref_models.Parameter.InsideAngle] comment added to overcome the problem with the comment map association
+	__AttributeShape__000003_InsideAngle.Identifier = `ref_models.Parameter.InsideAngle`
+	__AttributeShape__000003_InsideAngle.FieldTypeAsString = ``
+	__AttributeShape__000003_InsideAngle.Structname = `Parameter`
+	__AttributeShape__000003_InsideAngle.Fieldtypename = `float64`
+
+	__AttributeShape__000004_PitchDifference.Name = `PitchDifference`
+
+	//gong:ident [ref_models.Parameter.PitchDifference] comment added to overcome the problem with the comment map association
+	__AttributeShape__000004_PitchDifference.Identifier = `ref_models.Parameter.PitchDifference`
+	__AttributeShape__000004_PitchDifference.FieldTypeAsString = ``
+	__AttributeShape__000004_PitchDifference.Structname = `Parameter`
+	__AttributeShape__000004_PitchDifference.Fieldtypename = `int`
+
+	__AttributeShape__000005_BeatsPerSecond.Name = `BeatsPerSecond`
+
+	//gong:ident [ref_models.Parameter.BeatsPerSecond] comment added to overcome the problem with the comment map association
+	__AttributeShape__000005_BeatsPerSecond.Identifier = `ref_models.Parameter.BeatsPerSecond`
+	__AttributeShape__000005_BeatsPerSecond.FieldTypeAsString = ``
+	__AttributeShape__000005_BeatsPerSecond.Structname = `Parameter`
+	__AttributeShape__000005_BeatsPerSecond.Fieldtypename = `float64`
+
+	__AttributeShape__000006_IsMinor.Name = `IsMinor`
+
+	//gong:ident [ref_models.Parameter.IsMinor] comment added to overcome the problem with the comment map association
+	__AttributeShape__000006_IsMinor.Identifier = `ref_models.Parameter.IsMinor`
+	__AttributeShape__000006_IsMinor.FieldTypeAsString = ``
+	__AttributeShape__000006_IsMinor.Structname = `Parameter`
+	__AttributeShape__000006_IsMinor.Fieldtypename = `bool`
+
+	__AttributeShape__000007_ThemeBinaryEncoding.Name = `ThemeBinaryEncoding`
+
+	//gong:ident [ref_models.Parameter.ThemeBinaryEncoding] comment added to overcome the problem with the comment map association
+	__AttributeShape__000007_ThemeBinaryEncoding.Identifier = `ref_models.Parameter.ThemeBinaryEncoding`
+	__AttributeShape__000007_ThemeBinaryEncoding.FieldTypeAsString = ``
+	__AttributeShape__000007_ThemeBinaryEncoding.Structname = `Parameter`
+	__AttributeShape__000007_ThemeBinaryEncoding.Fieldtypename = `int`
+
+	__Classdiagram__000000_Default.Name = `Default`
+	__Classdiagram__000000_Default.IsInRenameMode = false
+	__Classdiagram__000000_Default.IsExpanded = true
+	__Classdiagram__000000_Default.NodeGongStructsIsExpanded = false
+	__Classdiagram__000000_Default.NodeGongStructNodeExpansionBinaryEncoding = 16384
+	__Classdiagram__000000_Default.NodeGongEnumsIsExpanded = false
+	__Classdiagram__000000_Default.NodeGongEnumNodeExpansionBinaryEncoding = 0
+	__Classdiagram__000000_Default.NodeGongNotesIsExpanded = false
+	__Classdiagram__000000_Default.NodeGongNoteNodeExpansionBinaryEncoding = 0
 
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.Name = `Diagram Package created the 2025-05-04T23:28:43Z`
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.Path = ``
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.GongModelPath = ``
 	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.AbsolutePathToDiagramPackage = ``
 
+	__GongStructShape__000000_Default_Parameter.Name = `Default-Parameter`
+	__GongStructShape__000000_Default_Parameter.X = 108.000000
+	__GongStructShape__000000_Default_Parameter.Y = 38.000000
+
+	//gong:ident [ref_models.Parameter] comment added to overcome the problem with the comment map association
+	__GongStructShape__000000_Default_Parameter.Identifier = `ref_models.Parameter`
+	__GongStructShape__000000_Default_Parameter.ShowNbInstances = false
+	__GongStructShape__000000_Default_Parameter.NbInstances = 0
+	__GongStructShape__000000_Default_Parameter.Width = 240.000000
+	__GongStructShape__000000_Default_Parameter.Height = 223.000000
+	__GongStructShape__000000_Default_Parameter.IsSelected = false
+
 	// Setup of pointers
+	// setup of AttributeShape instances pointers
+	// setup of Classdiagram instances pointers
+	__Classdiagram__000000_Default.GongStructShapes = append(__Classdiagram__000000_Default.GongStructShapes, __GongStructShape__000000_Default_Parameter)
 	// setup of DiagramPackage instances pointers
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.Classdiagrams = append(__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.Classdiagrams, __Classdiagram__000000_Default)
+	__DiagramPackage__000000_Diagram_Package_created_the_2025_05_04T23_28_43Z.SelectedClassdiagram = __Classdiagram__000000_Default
+	// setup of GongStructShape instances pointers
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000000_N)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000001_M)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000002_Z)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000003_InsideAngle)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000004_PitchDifference)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000005_BeatsPerSecond)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000006_IsMinor)
+	__GongStructShape__000000_Default_Parameter.AttributeShapes = append(__GongStructShape__000000_Default_Parameter.AttributeShapes, __AttributeShape__000007_ThemeBinaryEncoding)
 }
